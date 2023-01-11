bdcf4b9fb6c266d46b305aefb11339b1c03acbeb
Jens <jens@perun.network>
2023-01-11 14:49:02 +0100

    Re-export all the sub-types of channel
    The user/application should be able to create variables of this type or
    compare the returned errors
    
    Signed-off-by: Jens <jens@perun.network>

--- src/channel.rs
+++ src/channel.rs
@@ -8,12 +8,11 @@ mod signed;
 use crate::abiencode::types::{Address, Bytes32, U256};
 use serde::Serialize;
 
-// TODO: Re-export all other types, too
-pub use channel_update::ChannelUpdate;
-
-// Re-exported within this crate because the PerunClient has to be able to
-// create a new ProposedChannel, but the proposal module is private.
-pub(crate) use proposal::ProposedChannel;
+pub use agreed_upon::*;
+pub use channel_update::*;
+pub use channel_update::*;
+pub use proposal::*;
+pub use signed::*;
 
 // Re-exported because it is part of the low-level channel API
 pub use crate::messages::LedgerChannelProposal;
