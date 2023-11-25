df4f12c40b09806dcc0200f1dab016bc11a35223
Manoranjith <ponraj.manoranjitha@in.bosch.com>
2023-11-25 21:41:11 +0100

    Change Fund(Req/Resp) in proto def
    Signed-off-by: Manoranjith <ponraj.manoranjitha@in.bosch.com>

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &["wire.proto", "perun-remote.proto"],
+        &["wire.proto", "perun-remote.proto", "errors.proto"],
         &(["go-perun/wire/protobuf/", "src/wire/"]),
     )?;
     Ok(())
